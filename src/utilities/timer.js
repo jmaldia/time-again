c16802dc853d64fa341de904366dda4598458540
Jon Maldia <jon@maldia.net>
2018-09-08 21:21:02 -0400

    Fixed bug

--- src/utilities/timer.js
+++ src/utilities/timer.js
@@ -1,16 +1,25 @@
 let timer = 0;
+let counter;
 
-function myTime(minutes) {
-    for (let min = 0; min < minutes; min++) {
-        let countUp = setInterval(() => {
-            timer++;
-            console.log(timer);
+function myTime(repeatTimerCount) {
+    let countDown = repeatTimerCount;
 
-            if (timer === 5) {
-                clearInterval(countUp);
+    console.log(`The timer will repeat ${countDown} more times`);
+
+    let countUp = setInterval(() => {
+        timer++;
+        console.log(timer);
+
+        if (timer === 6) {
+            clearInterval(countUp);
+            timer = 0;
+            countDown--;
+            if (countDown > 0) {
+                myTime(countDown);
             }
-        }, 1000);
-    };
+        }
+    }, 1000);
 }
+
 
 myTime(5);
